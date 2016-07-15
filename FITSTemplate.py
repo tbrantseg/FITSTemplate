6a6ebc0760dfd6048c820601bd0f77f447a82bfc
Tom Brantseg <tbrantse@iastate.edu>
2016-07-15 14:57:31 -0500

    First commit

---
+++ FITSTemplate.py
@@ -0,0 +1,207 @@
+#!/opt/local/bin/python
+
+import sys
+import os
+from astropy.io import fits
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import numpy as np
+import scipy.ndimage.interpolation as spr
+from astropy import wcs
+import ConfigParser
+
+class FITSCore (object):
+
+    def __init__ (self,\
+                 paramfile="template.cfg",\
+                 xsize=None,\
+                 ysize=None,\
+                 RA=None,\
+                 dec=None,\
+                 px_size=None):
+        """
+        Class to hold basic data on creating a FITS template. This class must
+        be instantiated before any templates are generated.
+
+        Constructor Arguments:
+        ---------------------
+        paramfile: Name of the parameter file holding information on the FITS file.
+
+        If paramfile is not specified, other arguments may be given explicitly.
+        
+        xsize: Size of the FITS file, in pixels.
+        ysize: Size of the FITS file in pixels.
+        RA: Decimal representation of the target right ascension.
+        dec: Decimal representation of the target declination.
+        px_size: Angular size of the FITS pixels in degrees.
+
+        Note that if both paramfile *and* the explicit arguments are given,
+        the values in paramfile take priority.
+
+        Methods:
+        --------
+        DiskTemplate: Generate a circular source with constant intensity.
+        GaussianTemplate: Generate a circular source with a Gaussian intensity.
+        EllipticalGaussianTemplate: Generate an elliptical source with a Gaussian
+             intensity.
+        """
+        if (os.path.isfile(paramfile)):
+            config=ConfigParser.RawConfigParser()
+            config.read(paramfile)
+            self.xsize = config.getint("FOV","xsize")
+            self.ysize = config.getint("FOV","ysize")
+            self.RA_val = config.getfloat("FOV","RA_val")
+            self.dec_val = config.getfloat("FOV","dec_val")
+            self.px_size = config.getfloat("FOV","px_size")
+        else:
+            self.xsize = xsize
+            self.ysize = ysize
+            self.RA_val = RA
+            self.dec_val = dec
+            self.px_size = px_size
+        self.field=np.zeros((self.xsize,self.ysize))
+
+        
+    def _SaveToFITS (self, outname, overwrite):
+        w = wcs.WCS(naxis=2)
+        xvals = np.arange(0,self.xsize)
+        yvals = np.arange(0,self.ysize)
+        w.wcs.crval = [self.RA_val,self.dec_val]
+        w.wcs.cdelt = np.array([self.px_size*-1.0,self.px_size])
+        w.wcs.crpix = [self.xsize/2,self.ysize/2]
+        w.wcs.ctype = ["RA---TAN","DEC--TAN"]
+        header = w.to_header()
+        hdu = fits.PrimaryHDU(self.field,header=header)
+        try:
+            hdu.writeto(outname,clobber=overwrite)
+        except IOError:
+            print "Could not create file! {0} already exists. Try again with overwrite set to True.".format(outname)
+
+            
+    def DiskTemplate (self, radius, intensity, outname, overwrite=False):
+        """
+        Generate a circular source with constant intensity.
+
+        Function Call:
+        -------------
+        FITSCore.DiskTemplate(radius,intensity,outname)
+
+        Arguments:
+        ---------
+        radius: Radius of the disk, in pixels.
+        intensity: Intensity of the source, in arbitrary units.
+        outname: Name of the output file.
+        overwrite(optional): If the output file exists, should it be overwritten?
+             Defaults to False.
+        """
+        x_c = self.xsize/2
+        y_c = self.ysize/2
+        for x in range(0,self.xsize):
+            for y in range(0,self.ysize):
+                if ((x-x_c)**2 + (y-y_c) **2 <= radius**2):
+                    self.field[x][y] = intensity
+        if outname:
+            self._SaveToFITS(outname,overwrite)
+
+        
+    def SplitDiskTemplate(self, radius, intensity_a, intensity_b, split_angle, outname, overwrite=False):
+        """
+        Generate a disk split between two different intensities.
+
+        Function Call:
+        -------------
+        FITSCore.DiskTemplate(radius,intensity_a,intensity_b,split_angle,outname)
+
+        Arguments:
+        ---------
+        radius: Radius of the disk, in pixels.
+        intensity_a: Intensity of one half of the disk, in arbitrary units.
+        intensity_b: Intensity of the other half of the disk, in arbitrary units.
+        split_angle: Angle at which the dividing line between the two halves of the
+         disk will be, in degrees clockwise from straight north-south.
+        outname: File output name.
+        overwite: If the output file exists, should it be overwritten? Defaults to False.
+        """
+        x_c = self.xsize/2
+        y_c = self.ysize/2
+        temp_field = np.zeros
+        for x in range(0,self.xsize):
+            for y in range(0,self.ysize):
+                if (x-x_c)**2 + (y-y_c)**2 <= radius**2:
+                    if y >= y_c:
+                        self.field[x][y] = intensity_a
+                    elif y < y_c:
+                        self.field[x][y] = intensity_b
+        # Now rotate
+        spr.rotate(input = self.field,\
+                                        angle = split_angle,\
+                                        reshape = False,\
+                                        output = self.field)
+        # Kill any junk outside the intended radius
+        for x in range(0,self.xsize):
+            for y in range(0,self.ysize):
+                if (x-x_c)**2 + (y-y_c)**2 > radius**2:
+                    self.field[x][y] = 0
+        if outname:
+            self._SaveToFITS(outname,overwrite)
+
+            
+    def EllipticalGaussianTemplate(self, FWHM, peak, epsilon, theta, outname, overwrite=False):
+        """
+        Generate an elliptical source with a Gaussian intensity.
+
+        Function Call:
+        -------------
+        FITSCore.EllipticalGaussianTemplate(FWHM,peak,epsilon,theta,outname)
+
+        Arguments:
+        ---------
+        FWHM: Full-width half-maximum of the source, in pixels.
+        peak: Peak value of the source brightness.
+        epsilon: Eccentricity of the source. Throws ValueError if out of bounds
+             (>1 or <0).
+        theta: Source rotation angle, in degrees.
+        outname: Name of the output file.
+        overwrite(optional): If the output file exists, should it be overwritten?
+             Defaults to False.        
+        """
+        # Throw ValueError if we're out of bounds for the eccentricity.
+        if (epsilon > 1 or epsilon < 0):
+            raise ValueError('Epsilon must be a value between 0 and 1.')
+            
+        # As defined on the CXC Sherpa website.
+        x_c = self.xsize/2
+        y_c = self.ysize/2
+        FWHM=float(FWHM)
+        peak=float(peak)
+        theta=np.radians(theta)
+        gauss_c = 2.7725887 # 4 log 2; relationship between FWHM and sigma
+        ellip = float(1-epsilon)
+        for x in range(0,self.xsize):
+            for y in range(0,self.ysize):
+                x_o = (x-x_c)*np.cos(theta) + (y-y_c)*np.sin(theta)
+                y_o = (y-y_c)*np.cos(theta) - (x-x_c)*np.sin(theta)
+                r = np.sqrt(((x_o**2 * ellip**2) + y_o**2)/ellip)
+                self.field[x][y]=peak*np.e**(-gauss_c*(r/FWHM)**2)
+        self._SaveToFITS(outname, overwrite)
+
+        
+    def GaussianTemplate (self, FWHM, peak, outname,overwrite=False):
+        """
+        Generate a circular source with a Gaussian intensity.
+
+        Function Call:
+        -------------
+        FITSCore.GaussianTemplate(FWHM,peak,outname)
+
+        Arguments:
+        ---------
+        FWHM: Full-width half-maximum of the source, in pixels.
+        peak: Peak value of the source brightness.
+        outname: Name of the output file.
+        overwrite(optional): If the output file exists, should it be overwritten?
+             Defaults to False.
+        """
+        self.EllipticalGaussianTemplate(FWHM,peak,0,0,outname,overwrite)
+
+# FITSTemplate.py ends here
